fb9e05b7be1e4c9ea20a5c8abee6f48a9bcf1c64
Santiago Fernandez Pensa <sfernandez@SOFTWARE-FACTORY.ENCODESA.INTERNAL>
2024-06-05 12:56:09 -0300

    terminar spinner

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { LottieModule } from 'ngx-lottie';
 import player from 'lottie-web';
 
@@ -7,4 +7,29 @@ import player from 'lottie-web';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+  public miraalud: boolean = false;
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.miralud();
+    }, 3000);
+    this.close();
+  }
+
+  miralud() {
+    this.miraalud = false;
+  }
+
+  close() {
+    let component = document.getElementById('loader');
+    console.log(component);
+
+    // component?.classList.remove('fadein');
+    component?.classList.add('fadeout');
+    component?.classList.add('animation-duration-2000');
+    setTimeout(() => {
+      this.miraalud = true;
+    }, 1900);
+  }
+}
