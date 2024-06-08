b908269c7790d333ca54c0c3ed365a3b448075ed
Santiago Fernandez <santiagoferking@gmail.com>
2024-06-08 19:00:52 -0300

    Solucionando error de ngModel

--- src/app/out/pages/dashboard-invitado/dashboard-invitado.component.ts
+++ src/app/out/pages/dashboard-invitado/dashboard-invitado.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-dashboard-invitado',
   templateUrl: './dashboard-invitado.component.html',
   styleUrls: ['./dashboard-invitado.component.scss']
 })
 export class DashboardInvitadoComponent {
-
+  public date: Date | undefined;
+  public classSearch: string | undefined;
 }
--- src/app/primeng/primeng.module.ts
+++ src/app/primeng/primeng.module.ts
@@ -3,10 +3,13 @@ import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
 import { DividerModule } from 'primeng/divider';
 import { ScrollTopModule } from 'primeng/scrolltop';
+import { CardModule } from 'primeng/card';
+import { CalendarModule } from 'primeng/calendar';
+import { InputTextModule } from 'primeng/inputtext';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, ButtonModule, DividerModule, ScrollTopModule],
-  exports: [ButtonModule, DividerModule, ScrollTopModule],
+  imports: [CommonModule, ButtonModule, DividerModule, ScrollTopModule, CardModule, CalendarModule, InputTextModule],
+  exports: [ButtonModule, DividerModule, ScrollTopModule, CardModule, CalendarModule, InputTextModule],
 })
 export class PrimengModule {}
