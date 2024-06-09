67cc734591b536f9d582f1ac64fa5b3e6ae0914a
Santiago Fernandez <santiagoferking@gmail.com>
2024-06-09 19:25:22 -0300

    Seguir con la navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { PrimengModule } from './primeng/primeng.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms'; 
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule, PrimengModule, BrowserAnimationsModule],
+  imports: [BrowserModule, AppRoutingModule, SharedModule, PrimengModule, BrowserAnimationsModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
